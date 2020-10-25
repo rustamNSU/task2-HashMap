b586610bfa73a05dac6b9ad2c1f1771f2e9edeaa
Rustam <afaritovich@mail.ru>
2020-10-25 10:31:28 +0700

    add: declaration of HashMap class

---
+++ Test/HashMap.cpp
@@ -0,0 +1,10 @@
+#include "../HashMap.h"
+
+#include <iostream>
+#include <string>
+#include "gtest/gtest.h"
+
+TEST(HashMap, ConstructorTest){
+    HashMap< std::string, int> map1;
+    HashMap< std::string, int> map2(10);
+}
